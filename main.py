b5fc5852a7e0742cafe4efbceffef9c800c8cc53
benwilliams1205 <benwilliams1205@icloudcom>
2024-11-06 18:33:59 +0000

    main function

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+main()
